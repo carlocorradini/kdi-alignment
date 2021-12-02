52dcf58c36236666625c5ebcd6972924c1c58a41
Carlo <carlo.ita98@gmail.com>
2021-12-02 01:04:16 +0100

    oki

---
+++ src/kdigtfs.rs
@@ -0,0 +1,115 @@
+use gtfs_structures::{Availability, RouteType};
+use serde::Serialize;
+
+#[derive(Debug, Serialize)]
+#[serde(rename(serialize = "StopEnum"))]
+pub enum KdiStopEnum {
+    Generic,
+    BikeSharing,
+    BikeParking,
+    CarSharing,
+    Taxi,
+}
+
+#[derive(Debug, Serialize)]
+#[serde(rename(serialize = "WeelchairEnum"))]
+pub enum KdiWeelchairEnum {
+    Unknown,
+    Supported,
+    NotSupported,
+}
+
+impl From<Availability> for KdiWeelchairEnum {
+    fn from(availability: Availability) -> Self {
+        match availability {
+            Availability::Available => KdiWeelchairEnum::Supported,
+            Availability::NotAvailable => KdiWeelchairEnum::NotSupported,
+            _ => KdiWeelchairEnum::Unknown,
+        }
+    }
+}
+
+#[derive(Debug, Serialize)]
+#[serde(rename(serialize = "TransportEnum"))]
+pub enum KdiTransportEnum {
+    Tram,
+    Subway,
+    Rail,
+    Bus,
+    Ferry,
+    CableTram,
+    CableCar,
+    Funicular,
+    Trolleybus,
+    Monorail,
+}
+
+impl From<RouteType> for KdiTransportEnum {
+    fn from(route_type: RouteType) -> Self {
+        match route_type {
+            RouteType::Tramway => KdiTransportEnum::Tram,
+            RouteType::Subway => KdiTransportEnum::Subway,
+            RouteType::Rail => KdiTransportEnum::Rail,
+            RouteType::Bus => KdiTransportEnum::Bus,
+            RouteType::Ferry => KdiTransportEnum::Ferry,
+            RouteType::CableCar => KdiTransportEnum::CableTram,
+            RouteType::Gondola => KdiTransportEnum::CableCar,
+            RouteType::Funicular => KdiTransportEnum::Funicular,
+            RouteType::Other(11) => KdiTransportEnum::Trolleybus,
+            RouteType::Other(12) => KdiTransportEnum::Monorail,
+            _ => panic!("Unknown route type {:?}", route_type),
+        }
+    }
+}
+
+#[derive(Debug, Serialize)]
+#[serde(rename(serialize = "Agency"))]
+pub struct KdiAgency<'a> {
+    pub id: usize,
+    pub name: &'a str,
+    pub email: &'a str,
+    pub phone: &'a str,
+    pub url: &'a str,
+}
+
+#[derive(Debug, Serialize)]
+#[serde(rename(serialize = "Stop"))]
+pub struct KdiStop<'a> {
+    pub id: usize,
+    pub name: &'a str,
+    pub latitude: f64,
+    pub longitude: f64,
+    #[serde(rename(serialize = "type"))]
+    pub stype: KdiStopEnum,
+    pub weelchair: KdiWeelchairEnum,
+}
+
+#[derive(Debug, Serialize)]
+#[serde(rename(serialize = "Route"))]
+pub struct KdiRoute<'a> {
+    pub id: usize,
+    #[serde(rename(serialize = "agencyId"))]
+    pub agency_id: usize,
+    #[serde(rename(serialize = "shortName"))]
+    pub short_name: &'a str,
+    #[serde(rename(serialize = "longName"))]
+    pub long_name: &'a str,
+    pub transport: KdiTransportEnum,
+}
+
+#[derive(Debug, Serialize)]
+#[serde(rename(serialize = "Calendar"))]
+pub struct KdiCalendar {
+    pub id: usize,
+    #[serde(rename(serialize = "startDate"))]
+    pub start_date: String,
+    #[serde(rename(serialize = "endDate"))]
+    pub end_date: String,
+    pub monday: bool,
+    pub tuesday: bool,
+    pub wednesday: bool,
+    pub thursday: bool,
+    pub friday: bool,
+    pub saturday: bool,
+    pub sunday: bool,
+}
---
+++ src/main.rs
@@ -0,0 +1,195 @@
+mod kdigtfs;
+
+use chrono::{NaiveTime, Utc};
+use env_logger::{Builder, Target};
+use gtfs_structures::Gtfs;
+use log::{debug, info, LevelFilter};
+use std::error::Error;
+use std::fs;
+
+use crate::kdigtfs::{
+    KdiAgency, KdiCalendar, KdiRoute, KdiStop, KdiStopEnum, KdiTransportEnum, KdiWeelchairEnum,
+};
+
+const ALIGNEMENT_DIR: &'static str = "./alignment";
+const EXTRAURBAN_FILE: &'static str = "./data/extraurban.zip";
+const URBAN_FILE: &'static str = "./data/urban.zip";
+
+fn main() -> Result<(), Box<dyn Error>> {
+    // Initialize logger
+    Builder::new()
+        .target(Target::Stdout)
+        .filter_level(LevelFilter::Debug)
+        .init();
+
+    // Remove and recreate alignment directory
+    info!("Removing `{}` directory", ALIGNEMENT_DIR);
+    fs::remove_dir_all(ALIGNEMENT_DIR).ok();
+    info!("Creating `{}` directory", ALIGNEMENT_DIR);
+    fs::create_dir(ALIGNEMENT_DIR)?;
+
+    // Read GTFS files
+    info!("Reading `{}`", EXTRAURBAN_FILE);
+    let gtfs_extraurban = Gtfs::new(EXTRAURBAN_FILE)?;
+
+    info!("Reading `{}`", URBAN_FILE);
+    let gtfs_urban = Gtfs::new(URBAN_FILE)?;
+
+    // agency.txt
+    info!("Aligning `agency.txt`");
+    let agency: KdiAgency;
+
+    assert!(gtfs_extraurban.agencies.len() == 1);
+    assert!(gtfs_urban.agencies.len() == 1);
+    {
+        let gtfs_agency = gtfs_extraurban.agencies.first().unwrap();
+        agency = KdiAgency {
+            id: 1,
+            name: &gtfs_agency.name,
+            email: "info@trentinotrasporti.it",
+            phone: &gtfs_agency.phone.as_ref().unwrap(),
+            url: &gtfs_agency.url,
+        };
+    }
+
+    info!("Writing `agency.json` file");
+    fs::write(
+        format!("{}/agency.json", ALIGNEMENT_DIR),
+        serde_json::to_string(&agency)?,
+    )?;
+
+    // stops.txt
+    info!("Aligning `stops.txt`");
+    let mut stops: Vec<KdiStop> = Vec::new();
+
+    debug!("Aligning extraurban `stops.txt`");
+    for (_, stop) in &gtfs_extraurban.stops {
+        stops.push(KdiStop {
+            id: stop.id.parse::<usize>()?,
+            name: &stop.name,
+            latitude: stop.latitude.unwrap(),
+            longitude: stop.longitude.unwrap(),
+            stype: KdiStopEnum::Generic,
+            weelchair: KdiWeelchairEnum::from(stop.wheelchair_boarding),
+        });
+    }
+    stops.sort_by(|a, b| a.id.cmp(&b.id));
+
+    debug!("Aligning urban `stops.txt`");
+    let last_stop_id: usize = stops.last().unwrap().id;
+    for (_, stop) in &gtfs_urban.stops {
+        stops.push(KdiStop {
+            id: last_stop_id + stop.id.parse::<usize>()?,
+            name: &stop.name,
+            latitude: stop.latitude.unwrap(),
+            longitude: stop.longitude.unwrap(),
+            stype: KdiStopEnum::Generic,
+            weelchair: KdiWeelchairEnum::from(stop.wheelchair_boarding),
+        });
+    }
+    stops.sort_by(|a, b| a.id.cmp(&b.id));
+
+    info!("Writing `stops.json` file");
+    fs::write(
+        format!("{}/stops.json", ALIGNEMENT_DIR),
+        serde_json::to_string(&stops)?,
+    )?;
+
+    // routes.txt
+    info!("Aligning `routes.txt`");
+    let mut routes: Vec<KdiRoute> = Vec::new();
+
+    debug!("Aligning extraurban `routes.txt`");
+    for (_, route) in &gtfs_extraurban.routes {
+        routes.push(KdiRoute {
+            id: route.id.parse::<usize>()?,
+            agency_id: 1,
+            short_name: &route.short_name,
+            long_name: &route.long_name,
+            transport: KdiTransportEnum::from(route.route_type),
+        })
+    }
+    routes.sort_by(|a, b| a.id.cmp(&b.id));
+
+    debug!("Aligning urban `routes.txt`");
+    let last_route_id: usize = routes.last().unwrap().id;
+    for (_, route) in &gtfs_urban.routes {
+        routes.push(KdiRoute {
+            id: last_route_id + route.id.parse::<usize>()?,
+            agency_id: 1,
+            short_name: &route.short_name,
+            long_name: &route.long_name,
+            transport: KdiTransportEnum::from(route.route_type),
+        })
+    }
+    routes.sort_by(|a, b| a.id.cmp(&b.id));
+
+    info!("Writing `routes.json` file");
+    fs::write(
+        format!("{}/routes.json", ALIGNEMENT_DIR),
+        serde_json::to_string(&routes)?,
+    )?;
+
+    // calendar.txt
+    info!("Aligning `calendar.txt`");
+    let mut calendars: Vec<KdiCalendar> = Vec::new();
+
+    debug!("Aligning extraurban `calendar.txt`");
+    for (_, calendar) in &gtfs_extraurban.calendar {
+        calendars.push(KdiCalendar {
+            id: calendar.id.parse::<usize>()?,
+            start_date: calendar
+                .start_date
+                .and_time(NaiveTime::from_hms(0, 0, 0))
+                .format("%Y-%m-%dT%H:%M:%S")
+                .to_string(),
+            end_date: calendar
+                .end_date
+                .and_time(NaiveTime::from_hms(0, 0, 0))
+                .format("%Y-%m-%dT%H:%M:%S")
+                .to_string(),
+            monday: calendar.monday,
+            tuesday: calendar.tuesday,
+            wednesday: calendar.wednesday,
+            thursday: calendar.thursday,
+            friday: calendar.friday,
+            saturday: calendar.saturday,
+            sunday: calendar.sunday,
+        })
+    }
+    calendars.sort_by(|a, b| a.id.cmp(&b.id));
+
+    debug!("Aligning urban `calendar.txt`");
+    let last_calendar_id: usize = calendars.last().unwrap().id;
+    for (_, calendar) in &gtfs_urban.calendar {
+        calendars.push(KdiCalendar {
+            id: last_calendar_id + calendar.id.parse::<usize>()?,
+            start_date: calendar
+                .start_date
+                .and_time(NaiveTime::from_hms(0, 0, 0))
+                .format("%Y-%m-%dT%H:%M:%S")
+                .to_string(),
+            end_date: calendar
+                .end_date
+                .and_time(NaiveTime::from_hms(0, 0, 0))
+                .format("%Y-%m-%dT%H:%M:%S")
+                .to_string(),
+            monday: calendar.monday,
+            tuesday: calendar.tuesday,
+            wednesday: calendar.wednesday,
+            thursday: calendar.thursday,
+            friday: calendar.friday,
+            saturday: calendar.saturday,
+            sunday: calendar.sunday,
+        })
+    }
+    calendars.sort_by(|a, b| a.id.cmp(&b.id));
+
+    info!("Writing `calendar.json` file");
+    fs::write(
+        format!("{}/calendar.json", ALIGNEMENT_DIR),
+        serde_json::to_string(&calendars)?,
+    )?;
+
+    Ok(())
+}
